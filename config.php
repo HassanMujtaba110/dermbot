334e20d186b233bd1de614221a3d254f2ab7f504
Hassan Mujtaba <hassanmujtabamemon123@gmail.com>
2026-05-01 16:24:26 +0500

    Add configuration settings for DermBot

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+define('OPENAI_API_KEY', getenv('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'));
+define('FREE_SCAN_LIMIT', 3);
+define('APP_NAME', 'DermBot');
